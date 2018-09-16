094100ef3c09c86d9dd6d3be31bfff3f7529143f
oussama <oussama.bouroukba@gmail.com>
2018-09-16 12:00:17 +0100

    auth implementation

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <div >
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+
+    
+}
+</script>
+
+<style scoped>
+div{
+    height: 100px;
+    background-color: red
+}
+</style>
+
